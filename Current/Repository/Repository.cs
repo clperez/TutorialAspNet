b6ae2c95debdff5703cfed3af4dd43122f18efb8
Carlos L. Perez <carlillos34@gmail.com>
2020-02-04 14:50:10 +0100

    Added two additional solutions, Current one, and the one with the forms and the IoC working.

---
+++ Current/Repository/Repository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tutorial._01Cookies.Models;
+
+namespace Tutorial._01Cookies.Repository
+{
+    public class Repository : IRepositoryService
+    {
+        private List<GuestResponse> responses = new List<GuestResponse>();
+
+        public Repository()
+        {
+        }
+
+        public IEnumerable<GuestResponse> Responses {
+            get
+            {
+                return responses;
+            }
+        }
+        public void AddResponse(GuestResponse response)
+        {
+            responses.Add(response);
+        }
+    }
+}
