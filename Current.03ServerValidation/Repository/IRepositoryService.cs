2389f1a017efdf09ac10489e65f0db2fcaa94dce
Carlos L. Perez <carlillos34@gmail.com>
2020-02-05 13:41:19 +0100

    Validations to a form added.

---
+++ Current.03ServerValidation/Repository/IRepositoryService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tutorial._01Cookies.Models;
+
+namespace Tutorial._01Cookies.Repository
+{
+    public interface IRepositoryService 
+    {
+
+        IEnumerable<GuestResponse> Responses { get; }
+        void AddResponse(GuestResponse response);
+    }
+}
