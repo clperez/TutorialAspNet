794ad0bf96ed2ca5c1e8451b89f3fed5a3a8c910
Carlos L. Perez <carlillos34@gmail.com>
2020-02-05 15:41:52 +0100

    Names conforming to format Styles prototype.

---
+++ Tutorial.04-Styles/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Tutorial._01Cookies.Models;
+using Tutorial._01Cookies.Repository;
+
+namespace Tutorial._01Cookies.Controllers
+{
+    public class HomeController : Controller
+    {
+        IRepositoryService repoService = null;
+
+        public HomeController(IRepositoryService repositoryService)
+        {
+            repoService = repositoryService;
+        }
+
+        public IActionResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View("MyView");
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                repoService.AddResponse(guestResponse);
+                //return RedirectToAction(nameof(Index));
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public ViewResult ListResponses()
+        {
+            var exp = repoService.Responses.Where(r => r.WillAttend == true);
+            return View(exp);
+        }
+    }
+}
---
+++ Tutorial.04-Styles/Program.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Tutorial._01Cookies
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var currentDirectory = Directory.GetCurrentDirectory();
+
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+        }
+    }
+}
