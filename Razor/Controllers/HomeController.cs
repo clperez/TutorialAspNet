979ef3e6104f54fce592bf8625ea3bd00159a0ab
Carlos L. Perez <carlillos34@gmail.com>
2020-02-06 15:22:46 +0100

    Renaming to create several tutorials. Razor first tutorial.

---
+++ Razor/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Current2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Current2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            Product myProduct = new Product
+            {
+                ProductID = 1,
+                Name = "Kayak",
+                Description = "A boat for one person",
+                Category = "Watersports",
+                Price = 275M
+            };
+            return View(myProduct);
+        }
+    }
+}
