19d6dec74c98652ec8a4a095638ae7f530ccc36b
Carlos L. Perez <carlillos34@gmail.com>
2020-02-07 23:29:25 +0100

    css and js bundles

---
+++ Razor.04-Style-Css-Bundles/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Current2.Models;
+using Current2.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Current2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IProductRepository productRepository;
+
+        public HomeController(IProductRepository pr)
+        {
+            productRepository = pr;
+        }
+
+
+        public ViewResult Index()
+        {
+            return View(productRepository.Entities);
+        }
+    }
+}
---
+++ Razor.04-Style-Css-Bundles/Services/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using Current2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Current2.Services
+{
+    public interface IProductRepository : ISimpleRepository<Product>
+    {
+    }
+}
