a2137c3b784905c255aec5fc9806cf9d3b1cd460
Carlos L. Perez <carlillos34@gmail.com>
2020-02-07 13:21:26 +0100

    Addint additional step to the tutorial

---
+++ Razor.02-Layouts-ViewStart-foreach-if/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Current2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Current2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            Product[] array = {
+                new Product {Name = "Kayak", Price = 275M, Stock=2},
+                new Product {Name = "Lifejacket", Price = 48.95M, Stock=4},
+                new Product {Name = "Soccer ball", Price = 19.50M, Stock=4},
+                new Product {Name = "Corner flag", Price = 34.95M, Stock=1}
+                };
+            return View(array);
+        }
+    }
+}
