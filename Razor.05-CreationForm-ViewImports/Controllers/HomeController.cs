37a1c6a9923904bc48f1e87966d846a35591dd25
Carlos L. Perez <carlillos34@gmail.com>
2020-02-09 21:09:57 +0100

    Product Creation form

---
+++ Razor.05-CreationForm-ViewImports/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Current2.Models;
+using Current2.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Current2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IProductRepository productRepository;
+
+        public HomeController(IProductRepository pr)
+        {
+            productRepository = pr;
+        }
+
+
+        public ViewResult Index() => View(productRepository.Entities.Where(p => p?.Price < 50));
+
+        [HttpGet]
+        public IActionResult AddProduct() => View(new Product());
+        [HttpPost]
+        public IActionResult AddProduct(Product p)
+        {
+            productRepository.AddProduct(p);
+            return RedirectToAction("Index");
+        }
+    }
+}
